4d620de1878120460e3db6d707068005fe28030b
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-03-13 20:56:25 +0330

    added special effects.
    details:
    - added talk to me and work samples buttons
    - added a little close button just under the main menu

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import ApplicationMenu from "./menu";
 
 function main() {
   const menu_element = document.getElementById("menu"); 
-  const menu_btn = document.getElementById("menu-btn");
+  const menu_btns = document.querySelectorAll("[data-action='toggle-menu']");
   let menu_api = new ApplicationMenu(menu_element);
-  menu_btn.addEventListener("click", () => {
-    if (menu_element.classList.contains("active"))
-      menu_api.closeMenu();
-    else
-      menu_api.openMenu();
+  menu_btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      if (menu_element.classList.contains("active"))
+        menu_api.closeMenu();
+      else
+        menu_api.openMenu();
+    });
   });
 }; main();
