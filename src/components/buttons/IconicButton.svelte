ff5c54a344aaaf941103180fb99e8ca08b42d4a7
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-04-17 16:17:54 +0430

    update: IconicButton.svelte (add styles)
    details:
    - remove property class_list
    - add property style (str)
    - add style for the button and icons
    - wrpped text node between span tags

--- src/components/buttons/IconicButton.svelte
+++ src/components/buttons/IconicButton.svelte
@@ -1,14 +1,33 @@
 <script>
     export let icon_class;
     export let text;
-    export let class_list = [];
+    export let style;
 </script>
 
-<button class="{class_list.join(' ')}">
+<button class="iconic-btn" style={style}>
     <span class="fa fa-{icon_class} btn-icon"></span>
-    <span>{text}</span>
+    <span class="btn-text">{text}</span>
 </button>
 
 <style>
-
+    .iconic-btn {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        background-color: rgba(68, 255, 193, 0.397);
+        border: 0;
+        outline: none;
+        color: white;
+        cursor: pointer;
+        padding: 5px 8px;
+        border-radius: 8px;
+        transition: all .09s ease-out;
+    }
+    .iconic-btn:hover {
+        background-color: rgb(9, 218, 187);
+    }
+    .btn-icon {
+        font-size: 30px;
+        margin-right: 8px;
+    }
 </style>
