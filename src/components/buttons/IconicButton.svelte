dc58620580e69325d23d693f91a0c00aa17f476d
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-04-18 11:37:24 +0430

    update: IconicButton.svelte (add ripple effect)

--- src/components/buttons/IconicButton.svelte
+++ src/components/buttons/IconicButton.svelte
@@ -1,10 +1,37 @@
 <script>
+    import { onMount } from "svelte";
     export let icon_class;
     export let text;
     export let style;
+
+    let button;
+
+    function ripple(event) {
+        let {offsetX: x, offsetY: y} = event;
+        console.log(event);
+        let ripple_el = document.createElement("span");
+        ripple_el.style.backgroundColor= "rgba(255, 255, 255, 0.37)";
+        ripple_el.style.width = "1px";
+        ripple_el.style.height = "1px";
+        ripple_el.style.position = "absolute";
+        ripple_el.style.top = y+"px";
+        ripple_el.style.left = x+"px";
+        ripple_el.style.animation="ripple_grow 250ms";
+        ripple_el.style.backgroundColor = "white";
+        ripple_el.style.borderRadius="50%";
+
+        button.appendChild(ripple_el);
+        setTimeout(() => {
+            ripple_el.remove();
+        }, 300);
+    }
+
+    onMount(() => {
+        button.addEventListener("click", ripple);
+    });
 </script>
 
-<button class="iconic-btn" style={style}>
+<button class="iconic-btn" style={style} bind:this={button}>
     <span class="fa fa-{icon_class} btn-icon"></span>
     <span class="btn-text">{text}</span>
 </button>
@@ -14,7 +41,7 @@
         display: flex;
         align-items: center;
         flex-direction: row;
-        background-color: rgba(68, 255, 193, 0.397);
+        background-color: rgb(32, 204, 147);
         border: 0;
         outline: none;
         color: white;
@@ -22,9 +49,14 @@
         padding: 5px 8px;
         border-radius: 8px;
         transition: all .09s ease-out;
+        overflow: hidden;
+        position: relative;
     }
     .iconic-btn:hover {
         background-color: rgb(9, 218, 187);
+    }
+    .iconic-btn * {
+        pointer-events: none;
     }
     .btn-icon {
         font-size: 30px;
