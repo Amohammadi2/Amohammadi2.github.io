45fb594319c733fa2dc66b65f14835236aeababc
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-05 12:40:07 +0430

    add: timeout progress bar
    details:
    - created progress_bar_el which is a refrence to progress bar
    - add keyframes animation to `.not-progress`
    - add timeout function to delete the notification after specific amount of time

--- src/components/notifications/Notification.svelte
+++ src/components/notifications/Notification.svelte
@@ -1,14 +1,26 @@
 <script>
+    import { onMount } from "svelte";
     import NotificationAPI from "../../modules/notificationAPI";
 
     export let msg;
     export let type;
     export let pk;
+
+    let progress_bar_el;
+    onMount(() => {
+        //delete notification after 1 second
+        setTimeout(() => {
+            NotificationAPI.delete(pk);
+        }, 3000);
+    });
 </script>
 
 <div class="notification {type}">
     <span class="dismiss" on:click={() => NotificationAPI.delete(pk)}>X</span>
     <span> {msg} </span>
+
+    <div class="not-progress" bind:this={progress_bar_el}>
+    </div>
 </div>
 
 <style>
@@ -17,6 +29,7 @@
         box-sizing: border-box;
         padding: 10px 8px;
         margin: 5px 0px;
+        position: relative;
     }
 
     .notification.alert {
@@ -38,5 +51,29 @@
         margin-right: 3px;
         margin-left: 3px;
         cursor: pointer;
+    }
+
+    .not-progress {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right:0;
+        height: 3px; 
+    }
+    .not-progress::after {
+        content: '';
+        display: block;
+        height: 3px;
+        animation: progress_animation 3000ms forwards;
+        background-color: orange;
+    }
+
+    @keyframes progress_animation {
+        from { 
+            width: 0%;
+        }
+        to {
+            width: 100%;
+        }
     }
 </style>
