f08041f401ba2a78446ee5bc1dfe30a271dac8e6
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-07 20:13:45 +0430

    add: under development text

---
+++ src/routes/UnderDevelopment.svelte
@@ -0,0 +1,16 @@
+<div class="centeralize">
+    <div class="text-container">
+        این قسمت از وبسایت در حال توسعه است
+    </div>
+</div>
+
+<style>
+    .centeralize {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100%;
+        color: rgb(56, 56, 56);
+    }
+</style>
