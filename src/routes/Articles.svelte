47557325f66270b461c90909f8687ae76086214b
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-19 16:23:18 +0430

    feature: implement rss feed fetching system

---
+++ src/routes/Articles.svelte
@@ -0,0 +1,21 @@
+<script>
+    import PostCard from "../components/cards/PostCard.svelte";
+
+    async function getFeed() {
+        const response = await fetch("https://virgool.io/feed/@mohammadiashkan1384", {
+            mode: 'no-cors'
+        });
+        const text_rep = await response.text();
+        if (!response.ok)
+            return getFeed(); // repeat until one succeeds
+        console.log("getting the feed was successfull");
+        return text_rep;
+    }
+    let promise = getFeed();
+</script>
+
+{#await promise}
+    loading the feed... please wait;
+{:then feed} 
+    {feed}
+{/await}
