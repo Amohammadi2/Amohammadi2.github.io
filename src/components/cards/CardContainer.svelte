a4d3e5772f8ed8d7e8e22d4a5221f2f8b4496efe
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-30 14:06:27 +0430

    add: CardContainer.svelte

---
+++ src/components/cards/CardContainer.svelte
@@ -0,0 +1,24 @@
+<div class="card-container">
+    <div class="title">
+        <slot name="title"></slot>
+    </div>
+    
+    <slot name="cards"></slot>
+</div>
+
+<style>
+    .card-container {
+        box-sizing: border-box;
+        padding: 10px 25px;
+        background-color: white;
+        box-shadow: 0px 1px 15px 3px rgb(216, 216, 216);
+        margin: 12px 0px;
+        border-radius: 18px;
+    }
+
+    .card-container .title {
+        font-weight: bold;
+        font-size: 35px;
+        color: rgb(51, 51, 51);
+    }
+</style>
